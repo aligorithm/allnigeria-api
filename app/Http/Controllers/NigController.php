01c85506afcba86e113baf21345597a2f79f6b9b
Ali Yisa <aliyisa@outlook.com>
2019-06-26 15:15:58 +0100

    update docs

--- app/Http/Controllers/NigController.php
+++ app/Http/Controllers/NigController.php
@@ -10,7 +10,8 @@ use App\Ward;
  * @resource API
  *
  * This API aims to provide needed structured national data for use by developers and other individuals or organisations.
- * For now, data available includes all states, LGAs, Wards and Polling units correctly structured with the hierarchy. Feel free to use or contribute.
+ * For now, data available includes all states, LGAs, Wards and Polling units correctly structured with the hierarchy.
+ * Feel free to use or contribute.
  * Data is also provided as .sql or JSON
  * https://allnigeria-api.herokuapp.com/data/allnigeria.json <br>
  * https://allnigeria-api.herokuapp.com/data/allnigeria.sql
@@ -19,9 +20,9 @@ class NigController extends Controller
 {
     /**
      * Show Everything
-     * All data structured by states => LGAs => Wards => Polling Units will be returned. The size of this data is approximately 12MB
-     * This endpoint is disabled due to large memory usage. However, the JSON data is available via the link above
-     * If you wish to use this endpoint (possibly in a forked version), uncomment lines 26 and 28 of NigController and delete line 22.
+     * All data structured by states => LGAs => Wards => Polling Units will be returned. The size of this data is approximately 12MB.
+     * This endpoint is disabled due to large memory usage. However, the JSON data is available via the link above.
+     * If you wish to use this endpoint (possibly in a forked version), uncomment lines 29 and 32 of NigController and delete line 22.
      *
      */
     public function getAll(){
